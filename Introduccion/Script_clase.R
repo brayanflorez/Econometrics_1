d718bdd7ef118435c1ac943d860098415a36a705
David Florez <florezb464@gmail.com2>
2026-06-19 00:03:53 -0500

    cambios

---
+++ Introduccion/Script_clase.R
@@ -0,0 +1,16 @@
+# COMPLEMENTARIA 1
+
+install.packages("")
+
+
+#Ver donde estamos trabajando 
+getwd()
+
+#cambiar el directorio de trabajo
+setwd()
+
+#Operaciones aritméticas básicas
+2+3
+10-4
+17/4
+2{10}
